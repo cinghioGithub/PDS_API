d188983efce677015e73e6cca4b56bdbbfed02c1
Enrico <bravienrico@yahoo.it>
2021-04-16 12:30:35 +0200

    added direcotry lab_02

---
+++ lab_02/cmake-build-debug/CMakeFiles/lab_02.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab_02.dir/main.cpp.o"
+  "lab_02.exe"
+  "lab_02.pdb"
+  "liblab_02.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab_02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
