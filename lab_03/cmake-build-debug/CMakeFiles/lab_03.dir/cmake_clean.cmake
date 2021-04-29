5d4f93a250637c8c99405866d747f29951cf3943
Enrico <bravienrico@yahoo.it>
2021-04-29 17:22:12 +0200

    aggiunto progetto per lab03

---
+++ lab_03/cmake-build-debug/CMakeFiles/lab_03.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab_03.dir/main.cpp.o"
+  "lab_03.exe"
+  "lab_03.pdb"
+  "liblab_03.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab_03.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
