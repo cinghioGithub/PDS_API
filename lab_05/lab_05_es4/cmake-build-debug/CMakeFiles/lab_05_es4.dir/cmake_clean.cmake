797637f96449eef87fc78a0a9cb2fe0f649049b2
Enrico <bravienrico@yahoo.it>
2021-05-31 00:06:27 +0200

    added dir for es4

---
+++ lab_05/lab_05_es4/cmake-build-debug/CMakeFiles/lab_05_es4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab_05_es4.dir/main.cpp.o"
+  "lab_05_es4"
+  "lab_05_es4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab_05_es4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
